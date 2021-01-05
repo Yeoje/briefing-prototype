08317516c842c6124e9d0550ebe356716b7ec12c
Joe Buhrs <joe@buflr.co>
2021-01-05 16:39:22 +0100

    Added text to the Homepage AKA 'Nieuws van uw huisarts' Next up: Consult aanvraag.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -16,10 +16,59 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
+            <IonTitle size="large"><IonText color="primary"><h1>Nieuws van uw Huisarts</h1></IonText></IonTitle>
           </IonToolbar>
         </IonHeader>
-          <p>Home</p>
+        <div style={{padding: '5px'}}>
+          
+          <IonText color="tertiary"><h2>Vervangers</h2></IonText>
+            <h5>De volgende artsen worden tijdelijk vervangen:</h5>
+            <p>Dr. A Wimmel - vervangen door - Dr. C Stompe</p>
+            <p>Dr. R van Hooijdonck - vervangen door - Dr. A de Leven</p>
+
+          <IonText color="tertiary"><h2>Praktijk Nieuws</h2></IonText>
+            <h5>Alleen op afspraak</h5>
+            <p>Door de corona maatregelen zijn wij tijdelijk alleen op afspraak fysiek te bereiken. 
+              Dit geldt ook voor het ophalen van recepten en doorverwijzingen</p>
+
+            <h5>Richtlijnen bezoek</h5>
+            <p>U wordt tijdens uw bezoek verzocht de volgende maatregelen te nemen:</p>
+            
+              <ul>- doe een mondkapje op</ul>
+              <ul>- houdt 1.5m afstand</ul>
+              <ul>- ontsmet uw handen bij aankomst en vertrek</ul>
+              <ul>- blijf niet 'rondhangen'</ul>
+            
+            
+          <IonText color="tertiary"><h2>Weekdiensten</h2></IonText>
+            <table width="100%">
+              <tr>
+                <th>Dokter</th>
+                <th>Dagen</th>
+              </tr>
+              <tr>
+                <td>C. Stompe</td>
+                <td>Ma t/m Vr</td>
+              </tr>
+              <tr>
+                <td>A. de Leven</td>
+                <td>Ma t/m Vr</td>
+              </tr>
+              <tr>
+                <td>J. de Vries</td>
+                <td>Ma t/m Di - Do t/m Vr</td>
+              </tr>
+            </table>
+          <IonText color="tertiary"><h2>Griepprik en Gezondheidstips</h2></IonText>
+          <h5>Griepprik</h5>
+          <p>Vanaf oktober is er weer een griepprik mogelijkheid. 
+             De griepprik wordt gratis aangeboden aan kinderen en volwassenen die extra risico lopen om ernstig ziek te worden door griep.</p>
+
+          <h5>Vermijd droogte</h5>
+          <p>Blijf drinken en vermijd droge plekken. Deze herfst is het klimaat droger dan normaal wat huid irritaties kan
+            opleveren. Smeer hiervoor bijvoorbeeld wat zalf op de geirriteerde plek.
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
