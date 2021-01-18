6b880ff0c06ecfb8dcb31aa8a8c08fecf74fdf69
Joe Buhrs <joe@buflr.co>
2021-01-18 17:04:37 +0100

    Made a begin for the Chat page with help from Bossman

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Chat.css';
 
+interface Bericht {
+  mine: boolean,
+  text: string
+}
+
+const initialMessage: Bericht[] = [
+  {
+    mine: false,
+    text: "Goedemorgen, waarmee kan ik u helpen?"
+  }
+];
+
 const Chat: React.FC = () => {
+  const [berichten, setBerichten] = useState<Bericht[]>(initialMessage)
+  const [text, setText] = useState<string>("")
+
   return (
     <IonPage>
       <IonHeader>
@@ -22,7 +37,29 @@ const Chat: React.FC = () => {
             <IonTitle size="large">Chat</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <p>Chat met uw arts</p>
+        <div className="chat">
+        <ul>
+          {berichten?.map(bericht => (
+            <li className={bericht.mine ? "patient" : "dokter"}>
+              <p>{bericht.text}</p>
+            </li>
+          ))}
+        </ul>
+          <form onSubmit={e => {
+            e.preventDefault();
+            const bericht: Bericht = {
+              mine: true,
+              text
+            };
+            setBerichten([...berichten, bericht])
+            setText("");
+          }}>
+            <input type="text" required value={text} onChange={e => {
+              setText(e.target.value);
+            }} />
+            <IonButton type="submit">Verzenden</IonButton>
+          </form>
+        </div>
       </IonContent>
 
     </IonPage>
